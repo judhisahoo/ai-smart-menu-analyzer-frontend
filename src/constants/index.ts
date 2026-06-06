473ef73cd652516fc56724824219eabab59f50ae
Judhisthira Sahoo <judhisahoo@gmail.com>
2026-06-06 12:24:32 +0530

    some fixes in macbook

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,6 @@ export const BASE_URL = 'https://ai-restaurant-menu-analyzers-backen.vercel.app'
 
 //export const BASE_URL = 'https://ai-restaurant-menu-analyzers-backend.onrender.com'; // Replace with your actual API base URL
 
-//export const BASE_URL = 'http://192.168.1.106:3000'; // Replace with your actual API base URL for BLR
+//export const BASE_URL = 'http://192.168.1.111:3000'; // Replace with your actual API base URL for BLR
 
 //export const BASE_URL = 'http://192.168.1.108:3000'; // Replace with your actual API base URL for DKL
